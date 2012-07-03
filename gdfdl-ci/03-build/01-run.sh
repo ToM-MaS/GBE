bae621d1d3091a4878ecdfb0c0fb941514291389
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-07-03 13:04:14 +0200

    update FS build/install scripts

--- gdfdl-ci/03-build/01-run.sh
+++ gdfdl-ci/03-build/01-run.sh
@@ -30,8 +30,103 @@ if [ -f "${GDFDL_ENTRYWRAPPER}" ];
 	"${GDFDL_ENTRYWRAPPER}" chroot mkdir -p -m 777 "${GDFDL_DIR}/config/chroot_local-includes/usr/local/src"
 	"${GDFDL_ENTRYWRAPPER}" chroot mkdir -p -m 777 "${GDFDL_DIR}/config/chroot_local-includes/opt"
 
-	echo "GBE: Copying 3rd party source depdendencies into their places ..."
-	[ -d "${SRC_CACHE}" ] && cp -rpf "${SRC_CACHE}/"* "${INSTALLBASEDIR}${GDFDL_DIR}/config/chroot_local-includes/usr/local/src/"
+	# echo "GBE: Caching 3rd party dependencies ..."
+	# [ ! -d "${SRC_CACHE}" ] && mkdir -p "${SRC_CACHE}"
+	# # Download 3rd party modules if not cached already
+	# #
+	# # FS: loading autotalent for mod_ladspa
+	# [ ! -f "${SRC_CACHE}/autotalent-${FS3RD_ladspa_autotalent}.tar.gz" ] && wget -P "${SRC_CACHE}" -c -t 5 --waitretry=3 "http://web.mit.edu/tbaran/www/autotalent-${FS3RD_ladspa_autotalent}.tar.gz"
+	# # FS: loading Sangoma wanpipe driver for mod_freetdm
+	# [ ! -f "${SRC_CACHE}/wanpipe-${FS3RD_freetdm_sangoma_wanpipe}.tgz" ] && wget -P "${SRC_CACHE}" -c -t 5 --waitretry=3 "ftp://ftp.sangoma.com/linux/current_wanpipe/wanpipe-${FS3RD_freetdm_sangoma_wanpipe}.tgz"
+	# # FS: loading Sangoma ISDN driver for mod_freetdm
+	# [ ! -f "${SRC_CACHE}/libsng_isdn-${FS3RD_freetdm_sangoma_isdn}.i686.tgz" ] && wget -P "${SRC_CACHE}" -c -t 5 --waitretry=3 "ftp://ftp.sangoma.com/linux/libsng_isdn/libsng_isdn-${FS3RD_freetdm_sangoma_isdn}.i686.tgz"
+	# # FS: loading Sangoma SS7 driver for mod_freetdm
+	# [ ! -f "${SRC_CACHE}/libsng_ss7-4-${FS3RD_freetdm_sangoma_ss7}.i686.tgz" ] && wget -P "${SRC_CACHE}" -c -t 5 --waitretry=3 "ftp://ftp.sangoma.com/linux/libsng_ss7/libsng_ss7-4-${FS3RD_freetdm_sangoma_ss7}.i686.tgz"
+	# # FS: loading libpri driver for mod_freetdm
+	# [ ! -f "${SRC_CACHE}/libpri-${FS3RD_freetdm_libpri}.tar.gz" ] && wget -P "${SRC_CACHE}" -c -t 5 --waitretry=3 "http://downloads.asterisk.org/pub/telephony/libpri/releases/libpri-${FS3RD_freetdm_libpri}.tar.gz"
+	# # loading opal for mod_opal
+	# set +e
+	# if [ ! -d "${SRC_CACHE}/ptlib-${FS3RD_opal_ptlib}" ]
+	# 	then
+	#   	c=1
+	# 	while [[ $c -le 5 ]]
+	# 	do
+	# 		svn --non-interactive co "https://opalvoip.svn.sourceforge.net/svnroot/opalvoip/ptlib/${FS3RD_opal_ptlib}" "${SRC_CACHE}/ptlib-${FS3RD_opal_ptlib}"
+	# 		if [ "$?" -eq "0" ]; then
+	# 			break;
+	# 		else
+	# 			[[ $c -eq 5 ]] && exit 1
+	# 			(( c++ ))
+	# 			rm -rf "${SRC_CACHE}/ptlib-${FS3RD_opal_ptlib}"
+	# 			echo "$c. try in 3 seconds ..."
+	# 			sleep 3
+	# 		fi
+	# 	done
+	# fi
+	# 
+	# if [ ! -d "${SRC_CACHE}/opal-${FS3RD_opal}" ]
+	# 	then
+	# 	c=1
+	# 	while [[ $c -le 5 ]]
+	# 	do
+	# 		svn --non-interactive co "https://opalvoip.svn.sourceforge.net/svnroot/opalvoip/opal/${FS3RD_opal}" "${SRC_CACHE}/opal-${FS3RD_opal}"
+	# 		if [ "$?" -eq "0" ]; then
+	# 			break;
+	# 		else
+	# 			[[ $c -eq 5 ]] && exit 1
+	# 			(( c++ ))
+	# 			rm -rf "${SRC_CACHE}/opal-${FS3RD_opal}"
+	# 			echo "$c. try in 3 seconds ..."
+	# 			sleep 3
+	# 		fi
+	# 	done
+	# fi
+	# 
+	# # loading OpenR2 driver for mod_freetdm
+	# if [ ! -d "${SRC_CACHE}/openr2-${FS3RD_freetdm_openr2}" ]
+	# 	then
+	# 	c=1
+	# 	while [[ $c -le 5 ]]
+	# 	do
+	# 		svn --non-interactive co "http://openr2.googlecode.com/svn/${FS3RD_freetdm_openr2}/" "${SRC_CACHE}/openr2-${FS3RD_freetdm_openr2}"
+	# 		if [ "$?" -eq "0" ]; then
+	# 			break;
+	# 		else
+	# 			[[ $c -eq 5 ]] && exit 1
+	# 			(( c++ ))
+	# 			rm -rf "${SRC_CACHE}/openr2-${FS3RD_freetdm_openr2}"
+	# 			echo "$c. try in 3 seconds ..."
+	# 			sleep 3
+	# 		fi
+	# 	done
+	# fi
+	# 
+	# # loading DAHDI driver for mod_freetdm
+	# if [ ! -d "${SRC_CACHE}/dahdi-hfcs" ]
+	# 	then
+	# 	c=1
+	# 	while [[ $c -le 5 ]]
+	# 	do
+	# 		git clone --depth=0 "git://dahdi-hfcs.git.sourceforge.net/gitroot/dahdi-hfcs/dahdi-hfcs" "${SRC_CACHE}/dahdi-hfcs"
+	# 		if [ "$?" -eq "0" ]; then
+	# 			break;
+	# 		else
+	# 			[[ $c -eq 5 ]] && exit 1
+	# 			(( c++ ))
+	# 			rm -rf "${SRC_CACHE}/dahdi-hfcs"
+	# 			echo "$c. try in 3 seconds ..."
+	# 			sleep 3
+	# 		fi
+	# 	done
+	# fi
+	# set -e
+
+
+	if [[ -d "${SRC_CACHE}" ]]
+		then
+		echo "GBE: Copying 3rd party source depdendencies into their places ..."
+		cp -rpf "${SRC_CACHE}/"* "${INSTALLBASEDIR}${GDFDL_DIR}/config/chroot_local-includes/usr/local/src/"
+	fi
 
 	if [[ -d "${SRC_CACHE_BIN}" ]]
 		then
