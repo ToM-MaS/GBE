8a48c2b3ecb3960fdc3c903f979470301991d9bb
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-11-19 00:19:27 +0100

    Remove manual installation of FreeSwitch sounds and music files

--- config.v2/chroot_local-hooks/05-freeswitch.sh
+++ config.v2/chroot_local-hooks/05-freeswitch.sh
@@ -371,18 +371,6 @@ if [[ ! -f /usr/bin/freeswitch ]]
 		chown -R freeswitch.root /opt/freeswitch
 	fi
 else
-	# for Debian package based installations we still need to do some additional work for music and sounds
-	wget -P "${SRC_DIR}" -c -t 5 --waitretry=3 "http://files.freeswitch.org/freeswitch-sounds-en-us-callie-8000-1.0.16.tar.gz"
-	wget -P "${SRC_DIR}" -c -t 5 --waitretry=3 "http://files.freeswitch.org/freeswitch-sounds-en-us-callie-16000-1.0.16.tar.gz"
-	wget -P "${SRC_DIR}" -c -t 5 --waitretry=3 "http://files.freeswitch.org/freeswitch-sounds-music-8000-1.0.8.tar.gz"
-	wget -P "${SRC_DIR}" -c -t 5 --waitretry=3 "http://files.freeswitch.org/freeswitch-sounds-music-16000-1.0.8.tar.gz"
-	mkdir -p /usr/share/freeswitch/sounds
-	chmod 755 /usr/share/freeswitch/sounds
-	find /usr/local/src -name 'freeswitch-*.tar.gz' -type f -exec tar xfz {} -C /usr/share/freeswitch/sounds \;
-	chown -R root.root /usr/share/freeswitch/sounds
-	find /usr/share/freeswitch/sounds -type d -exec chmod 755 {} \;
-	find /usr/share/freeswitch/sounds -type f -exec chmod 644 {} \;
-
 	# compatibility with manual installation and GS default directories
 	mkdir -p /opt/freeswitch
 	ln -s /usr/share/freeswitch/sounds /opt/freeswitch/sounds
