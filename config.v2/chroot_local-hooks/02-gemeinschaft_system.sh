7937c08683049ab0cc1f798b5fb179b62e3ba809
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-11-21 16:47:13 +0100

    Changes to system users; use original GS5 repo for manual build
    - separate service and admin account with sudo rights
    - correct creation of service group (missed before)

--- config.v2/chroot_local-hooks/02-gemeinschaft_system.sh
+++ config.v2/chroot_local-hooks/02-gemeinschaft_system.sh
@@ -18,9 +18,16 @@ source /gdfdl.conf
 echo -e "\n###########################################################
 ## GBE: Gemeinschaft specific system configuration\n\n"
 
-echo -e "GBE: Create local user ${GS_USER} and set default password ...\n"
-useradd ${GS_USER} -U -m -s /bin/bash
-echo "${GS_USER}:${GS_PASSWORD}" | chpasswd
+echo -e "GBE: Create service group ${GS_GROUP} ...\n"
+groupadd -r -f ${GS_GROUP}
+
+echo -e "GBE: Create service account ${GS_USER} ...\n"
+useradd ${GS_USER} -N -m -r -s /bin/bash -c "Gemeinschaft Service Account" -g ${GS_GROUP}
+
+echo -e "GBE: Create admin account ${GS_USER} ...\n"
+useradd ${GS_USER_ADMIN} -N -m -s /bin/bash -c "Gemeinschaft Administrator Account" -g ${GS_GROUP}
+adduser ${GS_USER_ADMIN} sudo
+echo "${GS_USER_ADMIN}:${GS_PASSWORD_ADMIN}" | chpasswd
 
 echo -e "GBE: Correcting file permissions ...\n"
 chmod 0440 /etc/sudoers.d/*
