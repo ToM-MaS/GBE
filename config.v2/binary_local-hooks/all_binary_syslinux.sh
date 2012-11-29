5940ffaa1e1137a596d586d7e8e6e84325fe7d5f
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-11-29 11:25:50 +0100

    correct path relations

--- config.v2/binary_local-hooks/all_binary_syslinux.sh
+++ config.v2/binary_local-hooks/all_binary_syslinux.sh
@@ -12,9 +12,9 @@ LB_BOOTAPPEND_LIVE_CH="timezone=Europe/Zurich locales=de_CH.UTF-8 keyboard-layou
 LB_BOOTAPPEND_LIVE_EN="timezone=Europe/London locales=en_US.UTF-8 keyboard-layouts=us"
 
 # Copy default files
-cp -pf binary_syslinux/splash.png binary/isolinux/
-cp -pf binary_syslinux/stdmenu.cfg binary/isolinux/
-cp -pf binary_syslinux/menu.cfg binary/isolinux/
+cp -pf ../config/binary_syslinux/splash.png ../binary/isolinux/
+cp -pf ../config/binary_syslinux/stdmenu.cfg ../binary/isolinux/
+cp -pf ../config/binary_syslinux/menu.cfg ../binary/isolinux/
 
 # Live boot menu
 sed -e "s|@KERNEL@|/live/vmlinuz|g" \
@@ -22,10 +22,10 @@ sed -e "s|@KERNEL@|/live/vmlinuz|g" \
     -e "s|@LB_BOOTAPPEND_LIVE_DE@|${LB_BOOTAPPEND_LIVE} ${LB_BOOTAPPEND_LIVE_DE}|g" \
     -e "s|@LB_BOOTAPPEND_LIVE_CH@|${LB_BOOTAPPEND_LIVE} ${LB_BOOTAPPEND_LIVE_CH}|g" \
     -e "s|@LB_BOOTAPPEND_LIVE_EN@|${LB_BOOTAPPEND_LIVE} ${LB_BOOTAPPEND_LIVE_EN}|g" \
-    binary_syslinux/live.cfg.in > binary/isolinux/live.cfg
+    ../config/binary_syslinux/live.cfg.in > ../binary/isolinux/live.cfg
 
 # Install menu
 sed -e "s|@KERNEL@|/install/gtk/vmlinuz|g" \
     -e "s|@INITRD@|/install/gtk/initrd.gz|g" \
     -e "s|@LB_BOOTAPPEND_INSTALL@|video=vesa:ywrap,mtrr vga=788 quiet ${LB_BOOTAPPEND_INSTALL}|g" \
-    binary_syslinux/install.cfg.in > binary/isolinux/install.cfg
+    ../config/binary_syslinux/install.cfg.in > ../binary/isolinux/install.cfg
