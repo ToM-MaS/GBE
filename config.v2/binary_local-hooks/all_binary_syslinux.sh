ce76bb35f7220cb097401f88267ad8e5f6721c41
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-11-29 11:40:48 +0100

    be more verbose

--- config.v2/binary_local-hooks/all_binary_syslinux.sh
+++ config.v2/binary_local-hooks/all_binary_syslinux.sh
@@ -12,11 +12,13 @@ LB_BOOTAPPEND_LIVE_CH="timezone=Europe/Zurich locales=de_CH.UTF-8 keyboard-layou
 LB_BOOTAPPEND_LIVE_EN="timezone=Europe/London locales=en_US.UTF-8 keyboard-layouts=us"
 
 # Copy default files
+echo "Copy files from `pwd`/../config/binary_syslinux/ to `pwd`/../binary/isolinux/ ..."
 cp -pf ../config/binary_syslinux/splash.png ../binary/isolinux/
 cp -pf ../config/binary_syslinux/stdmenu.cfg ../binary/isolinux/
 cp -pf ../config/binary_syslinux/menu.cfg ../binary/isolinux/
 
 # Live boot menu
+echo "Generating `pwd`/../binary/isolinux/live.cfg based on  to `pwd`/../config/binary_syslinux/live.cfg.in ..."
 sed -e "s|@KERNEL@|/live/vmlinuz|g" \
     -e "s|@INITRD@|/live/initrd.gz|g" \
     -e "s|@LB_BOOTAPPEND_LIVE_DE@|${LB_BOOTAPPEND_LIVE} ${LB_BOOTAPPEND_LIVE_DE}|g" \
@@ -25,6 +27,7 @@ sed -e "s|@KERNEL@|/live/vmlinuz|g" \
     ../config/binary_syslinux/live.cfg.in > ../binary/isolinux/live.cfg
 
 # Install menu
+echo "Generating `pwd`/../binary/isolinux/install.cfg based on  to `pwd`/../config/binary_syslinux/install.cfg.in ..."
 sed -e "s|@KERNEL@|/install/gtk/vmlinuz|g" \
     -e "s|@INITRD@|/install/gtk/initrd.gz|g" \
     -e "s|@LB_BOOTAPPEND_INSTALL@|video=vesa:ywrap,mtrr vga=788 quiet ${LB_BOOTAPPEND_INSTALL}|g" \
