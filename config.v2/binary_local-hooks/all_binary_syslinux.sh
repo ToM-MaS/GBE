b44553b7259b40f887712d04362d9beef88af864
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-11-29 12:05:26 +0100

    improved path handling

--- config.v2/binary_local-hooks/all_binary_syslinux.sh
+++ config.v2/binary_local-hooks/all_binary_syslinux.sh
@@ -10,25 +10,26 @@
 LB_BOOTAPPEND_LIVE_DE="timezone=Europe/Berlin locales=de_DE.UTF-8 keyboard-layouts=de"
 LB_BOOTAPPEND_LIVE_CH="timezone=Europe/Zurich locales=de_CH.UTF-8 keyboard-layouts=ch"
 LB_BOOTAPPEND_LIVE_EN="timezone=Europe/London locales=en_US.UTF-8 keyboard-layouts=us"
+SELF="`pwd`"
 
 # Copy default files
-echo "Copy files from `pwd`/../config/binary_syslinux/ to `pwd`/../binary/isolinux/ ..."
-cp -pf ../config/binary_syslinux/splash.png ../binary/isolinux/
-cp -pf ../config/binary_syslinux/stdmenu.cfg ../binary/isolinux/
-cp -pf ../config/binary_syslinux/menu.cfg ../binary/isolinux/
+echo "Copy files from ${SELF}/config/binary_syslinux/ to ${SELF}/binary/isolinux/ ..."
+cp -pf ${SELF}/config/binary_syslinux/splash.png ${SELF}/binary/isolinux/
+cp -pf ${SELF}/config/binary_syslinux/stdmenu.cfg ${SELF}/binary/isolinux/
+cp -pf ${SELF}/config/binary_syslinux/menu.cfg ${SELF}/binary/isolinux/
 
 # Live boot menu
-echo "Generating `pwd`/../binary/isolinux/live.cfg based on  to `pwd`/../config/binary_syslinux/live.cfg.in ..."
+echo "Generating ${SELF}/binary/isolinux/live.cfg based on ${SELF}/config/binary_syslinux/live.cfg.in ..."
 sed -e "s|@KERNEL@|/live/vmlinuz|g" \
     -e "s|@INITRD@|/live/initrd.gz|g" \
     -e "s|@LB_BOOTAPPEND_LIVE_DE@|${LB_BOOTAPPEND_LIVE} ${LB_BOOTAPPEND_LIVE_DE}|g" \
     -e "s|@LB_BOOTAPPEND_LIVE_CH@|${LB_BOOTAPPEND_LIVE} ${LB_BOOTAPPEND_LIVE_CH}|g" \
     -e "s|@LB_BOOTAPPEND_LIVE_EN@|${LB_BOOTAPPEND_LIVE} ${LB_BOOTAPPEND_LIVE_EN}|g" \
-    ../config/binary_syslinux/live.cfg.in > ../binary/isolinux/live.cfg
+    ${SELF}/config/binary_syslinux/live.cfg.in > ${SELF}/binary/isolinux/live.cfg
 
 # Install menu
-echo "Generating `pwd`/../binary/isolinux/install.cfg based on  to `pwd`/../config/binary_syslinux/install.cfg.in ..."
+echo "Generating ${SELF}/binary/isolinux/install.cfg based on ${SELF}/config/binary_syslinux/install.cfg.in ..."
 sed -e "s|@KERNEL@|/install/gtk/vmlinuz|g" \
     -e "s|@INITRD@|/install/gtk/initrd.gz|g" \
     -e "s|@LB_BOOTAPPEND_INSTALL@|video=vesa:ywrap,mtrr vga=788 quiet ${LB_BOOTAPPEND_INSTALL}|g" \
-    ../config/binary_syslinux/install.cfg.in > ../binary/isolinux/install.cfg
+    ${SELF}/config/binary_syslinux/install.cfg.in > ${SELF}/binary/isolinux/install.cfg
