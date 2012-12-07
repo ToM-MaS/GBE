4eea84a845f34275b6a515f8e02958d86b4339b6
Julian Pawlowski <julian.pawlowski@gmail.com>
2012-12-07 01:01:42 +0100

    let mon_ami run as normal user instead of root

--- config.v2/chroot_local-hooks/06-gs5.sh
+++ config.v2/chroot_local-hooks/06-gs5.sh
@@ -259,7 +259,10 @@ a2enmod passenger 2>&1
 a2dissite default 2>&1
 a2ensite gemeinschaft 2>&1
 
-echo -e "GBE: Setting up permissions ...\n"
+echo -e "GBE: Setup runtime user for MonAMI ...\n"
+sed -i "s/^USER=.*/USER=\"${GS_USER}\"" /etc/init.d/mon_ami
+
+echo -e "GBE: Setting permissions ...\n"
 chown -R "${GS_USER}"."${GS_GROUP}" "${GS_DIR}" /var/log/gemeinschaft
 # Allow members of the GS system group to modify+upgrade files
 chmod -R g+w "${GS_DIR}"
