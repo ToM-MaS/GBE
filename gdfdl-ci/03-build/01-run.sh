5f8327b0f009f36185e75c42bbf121db0d1f30a6
Julian Pawlowski <julian.pawlowski@gmail.com>
2013-03-06 20:41:01 +0100

    http_proxy support for CI builds

--- gdfdl-ci/03-build/01-run.sh
+++ gdfdl-ci/03-build/01-run.sh
@@ -156,6 +156,20 @@ if [ -f "${GDFDL_ENTRYWRAPPER}" ];
 	"${GDFDL_ENTRYWRAPPER}" chroot chmod -f 755 "${GDFDL_DIR}/config/includes.chroot/etc"
 	"${GDFDL_ENTRYWRAPPER}" chroot chmod -f 644 "${GDFDL_DIR}/config/includes.chroot/etc/gemeinschaft_branch"
 	"${GDFDL_ENTRYWRAPPER}" chroot chown -f root.root "${GDFDL_DIR}/config/includes.chroot/etc/gemeinschaft_branch"
+
+	echo "GBE: Updating environment variables ..."
+	"${GDFDL_ENTRYWRAPPER}" chroot rm -rf "${GDFDL_DIR}/config/includes.chroot/etc/environment"
+	"${GDFDL_ENTRYWRAPPER}" chroot chmod -f 777 "${GDFDL_DIR}/config/includes.chroot/etc"
+	touch "${INSTALLBASEDIR}${GDFDL_DIR}/config/includes.chroot/etc/environment"
+	if [ x"$http_proxy" != x"" ]; then
+		echo "export http_proxy=$http_proxy" >> > "${INSTALLBASEDIR}${GDFDL_DIR}/config/includes.chroot/etc/environment"
+	fi
+	if [ x"$ftp_proxy" != x"" ]; then
+		echo "export ftp_proxy=$ftp_proxy" >> > "${INSTALLBASEDIR}${GDFDL_DIR}/config/includes.chroot/etc/environment"
+	fi
+	"${GDFDL_ENTRYWRAPPER}" chroot chmod -f 755 "${GDFDL_DIR}/config/includes.chroot/etc"
+	"${GDFDL_ENTRYWRAPPER}" chroot chmod -f 644 "${GDFDL_DIR}/config/includes.chroot/etc/environment"
+	"${GDFDL_ENTRYWRAPPER}" chroot chown -f root.root "${GDFDL_DIR}/config/includes.chroot/etc/environment"
 else
 	echo "ERROR: No existing build environment installation found. Run installer first."
 	exit 1
